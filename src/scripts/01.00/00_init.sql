ff7188f5370894ddebd3bfdcbc7c69ef3eb06ae1
Marco Nova <novamarco@hotmail.com>
2012-03-09 02:05:17 +0100

    Inizialization script for thr schemaversion table

---
+++ src/scripts/01.00/00_init.sql
@@ -0,0 +1,15 @@
+-- Schema version table
+-- let me see this second comment
+
+CREATE TABLE `schemaversion` (
+  `id` INT  NOT NULL AUTO_INCREMENT,
+  `major` INT  NOT NULL,
+  `minor` INT  NOT NULL,
+  `date` DATETIME  NOT NULL,
+  `script` VARCHAR(255),
+  `description` VARCHAR(255),
+  `md5` VARCHAR(255),
+  PRIMARY KEY (`id`)
+)
+ENGINE = InnoDB;
+
